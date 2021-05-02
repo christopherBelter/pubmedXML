901c4e47ffdf8afc32ea9cf98348c48ea9333d7d
Pachamaltese <mvargas@dcc.uchile.cl>
2021-05-02 01:09:30 -0400

    fixes year "2006 summer", "2020-03", etc

--- pubmedXML.R
+++ pubmedXML.R
@@ -34,6 +34,7 @@ extract_xml <- function(theFile) {
 	year <- lapply(records, xpathSApply, ".//PubDate/Year", xmlValue) 
 	year[sapply(year, is.list)] <- NA
 	year[which(sapply(year, is.na) == TRUE)] <- lapply(records[which(sapply(year, is.na) == TRUE)], xpathSApply, ".//PubDate/MedlineDate", xmlValue)
+	year <- gsub("[a-z]|[A-Z]|-.*|\\/.*|\\s+", "", year)
 	year <- gsub(" .+", "", year)
 	year <- gsub("-.+", "", year)
 	articletitle <- lapply(records, xpathSApply, ".//ArticleTitle", xmlValue) 
